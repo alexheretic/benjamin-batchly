63d33af3b9a9c1f02ae0e88c1162dddf54160b93
Alex Butler <alexheretic@gmail.com>
2022-06-02 15:32:36 +0100

    Add return values & document
    Trim dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,68 @@
-use futures::future::{self, Either};
+//! Low latency batching tool.
+//! Bundle lots of single concurrent operations into sequential batches of work.
+//!
+//! For example many concurrent contending database update tasks can be bundled
+//! into bulk updates for each item in a batch.
+//!
+//! # Example
+//! ```
+//! use benjamin_batchly::{BatchMutex, BatchResult};
+//!
+//! # async fn example() -> Result<(), &'static str> {
+//! let batcher = BatchMutex::default();
+//!
+//! # let (batch_key, item) = (1, 2);
+//! # async fn db_bulk_insert(_: &[i32]) -> Result<(), &'static str> { Ok(()) }
+//! // BatchMutex synchronizes so only one `Work` happens at a time (for a given batch_key).
+//! // All concurrent submissions made while an existing `Work` is being processed will
+//! // await completion and form the next `Work` batch.
+//! match batcher.submit(batch_key, item).await {
+//!     BatchResult::Work(mut batch) => {
+//!         db_bulk_insert(&batch.items).await?;
+//!         batch.notify_all_done();
+//!         Ok(())
+//!     }
+//!     BatchResult::Done(_) => Ok(()),
+//!     BatchResult::Failed => Err("failed"),
+//! }
+//! # }
+//! ```
+//!
+//! # Example: Return values
+//! Each item may also received it's own return value inside [`BatchResult::Done`].
+//!
+//! E.g. a `Result` to pass back why some batch items failed to their submitters.
+//! ```
+//! use benjamin_batchly::{BatchMutex, BatchResult};
+//! use anyhow::anyhow;
+//!
+//! # async fn example() -> anyhow::Result<()> {
+//! let batcher: BatchMutex<_, _, anyhow::Result<()>> = BatchMutex::default();
+//!
+//! # let (batch_key, my_item) = (1, 2);
+//! # async fn db_bulk_insert(_: &[i32]) -> Vec<(usize, bool)> { <_>::default() }
+//! match batcher.submit(batch_key, my_item).await {
+//!     BatchResult::Work(mut batch) => {
+//!         let results = db_bulk_insert(&batch.items).await;
+//!
+//!         // iterate over results and notify each item's submitter
+//!         for (index, success) in results {
+//!             if success {
+//!                 batch.notify_done(index, Ok(()));
+//!             } else {
+//!                 batch.notify_done(index, Err(anyhow!("insert failed")));
+//!             }
+//!         }
+//!
+//!         // receive the local `my_item` return value
+//!         batch.recv_local_notify_done().unwrap()
+//!     }
+//!     BatchResult::Done(result) => result,
+//!     BatchResult::Failed => Err(anyhow!("batch failed")),
+//! }
+//! # }
+//! ```
+use futures_util::future::{self, Either};
 use std::{
     collections::{hash_map::Entry, HashMap},
     fmt,
@@ -8,12 +72,17 @@ use std::{
 };
 use tokio::sync::{oneshot, OwnedMutexGuard};
 
+type BatchMap<K, V, T> = HashMap<K, BatchState<K, V, T>>;
+
+/// Batch HQ. Share and use concurrently to dynamically batch submitted items.
+///
+/// Cheap to clone (`Arc` guts).
 #[derive(Clone)]
-pub struct BatchMutex<Key: Eq + Hash, Item> {
-    queue: Arc<std::sync::Mutex<HashMap<Key, BatchState<Key, Item>>>>,
+pub struct BatchMutex<Key: Eq + Hash, Item, T = ()> {
+    queue: Arc<std::sync::Mutex<BatchMap<Key, Item, T>>>,
 }
 
-impl<Key: Eq + Hash, Item> Default for BatchMutex<Key, Item> {
+impl<Key: Eq + Hash, Item, T> Default for BatchMutex<Key, Item, T> {
     fn default() -> Self {
         Self {
             queue: <_>::default(),
@@ -21,11 +90,52 @@ impl<Key: Eq + Hash, Item> Default for BatchMutex<Key, Item> {
     }
 }
 
-impl<Key, Item> BatchMutex<Key, Item>
+impl<Key, Item, T> fmt::Debug for BatchMutex<Key, Item, T>
+where
+    Key: Eq + Hash,
+    Item: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("BatchMutex").finish_non_exhaustive()
+    }
+}
+
+impl<Key, Item, T> BatchMutex<Key, Item, T>
 where
     Key: Eq + Hash + Clone,
 {
-    pub async fn submit(&self, batch_key: Key, item: Item) -> BatchResult<Key, Item> {
+    /// Submits an `item` to be processed as a batch, ie with other items
+    /// submitted with the same `batch_key`.
+    ///
+    /// Submissions made while a previous `batch_key` batch is processing will await
+    /// that batch finishing. All such waiting items become the next batch as soon
+    /// as previous finishes.
+    ///
+    /// Note since submission adds no artificial latency the very first call will
+    /// always result in a batch of a single item.
+    ///
+    /// # Example
+    /// ```
+    /// # use benjamin_batchly::{BatchMutex, BatchResult};
+    /// # async fn example() -> Result<(), &'static str> {
+    /// # let batcher = BatchMutex::default();
+    /// # let (batch_key, item) = (1, 2);
+    /// # async fn db_bulk_insert(_: &[i32]) -> Result<(), &'static str> { Ok(()) }
+    /// // Synchronizes so only one `Work` happens at a time (for a given batch_key).
+    /// // All concurrent submissions made while an existing `Work` is being processed will
+    /// // await completion and form the next `Work` batch.
+    /// match batcher.submit(batch_key, item).await {
+    ///     BatchResult::Work(mut batch) => {
+    ///         db_bulk_insert(&batch.items).await?;
+    ///         batch.notify_all_done();
+    ///         Ok(())
+    ///     }
+    ///     BatchResult::Done(_) => Ok(()),
+    ///     BatchResult::Failed => Err("failed"),
+    /// }
+    /// # }
+    /// ```
+    pub async fn submit(&self, batch_key: Key, item: Item) -> BatchResult<Key, Item, T> {
         let (tx, rx) = tokio::sync::oneshot::channel();
         let batch_lock = {
             let mut queue = self.queue.lock().unwrap();
@@ -39,7 +149,7 @@ where
             Either::Left((result, guard)) => {
                 drop(guard);
                 match result {
-                    Ok(Ok(_)) => BatchResult::Done,
+                    Ok(Ok((val, _))) => BatchResult::Done(val),
                     Err(_) | Ok(Err(_)) => BatchResult::Failed,
                 }
             }
@@ -52,34 +162,31 @@ where
                     .map(move |state| Batch {
                         guard: Some(guard),
                         items: mem::take(&mut state.items),
-                        senders: mem::take(&mut state.senders),
+                        senders: state.senders.drain(..).map(Some).collect(),
                         cleaner: Cleaner {
                             queue: Arc::clone(&self.queue),
                             key: Some(batch_key),
                         },
-                    });
+                        local_rx: rx,
+                    })
+                    .unwrap(); // should always exist in queue at this point
 
-                match batch {
-                    Some(batch) => BatchResult::Work(batch),
-                    None => match rx.await {
-                        Ok(Ok(_)) => BatchResult::Done,
-                        Err(_) | Ok(Err(_)) => BatchResult::Failed,
-                    },
-                }
+                BatchResult::Work(batch)
             }
         }
     }
 }
 
-struct BatchState<Key: Eq + Hash, Item> {
+struct BatchState<Key: Eq + Hash, Item, T> {
     items: Vec<Item>,
-    senders: Vec<Sender<Key, Item>>,
+    senders: Vec<Sender<Key, Item, T>>,
     lock: Arc<tokio::sync::Mutex<()>>,
 }
 
-type Sender<K, V> = oneshot::Sender<Result<Cleaner<K, V>, Cleaner<K, V>>>;
+type Sender<K, V, T> = oneshot::Sender<Result<(T, Cleaner<K, V, T>), Cleaner<K, V, T>>>;
+type Receiver<K, V, T> = oneshot::Receiver<Result<(T, Cleaner<K, V, T>), Cleaner<K, V, T>>>;
 
-impl<Key: Eq + Hash, Item> Default for BatchState<Key, Item> {
+impl<Key: Eq + Hash, Item, T> Default for BatchState<Key, Item, T> {
     fn default() -> Self {
         Self {
             items: <_>::default(),
@@ -89,23 +196,38 @@ impl<Key: Eq + Hash, Item> Default for BatchState<Key, Item> {
     }
 }
 
+/// [`BatchMutex::submit`] output.
+/// Either a batch of items to process & notify or the result of another submitter
+/// handling a batch with the same `batch_key`.
 #[must_use]
 #[derive(Debug)]
-pub enum BatchResult<Key: Eq + Hash + Clone, Item> {
-    Done,
+pub enum BatchResult<Key: Eq + Hash + Clone, Item, T> {
+    /// A non-empty batch of items including the one submitted locally.
+    ///
+    /// All [`Batch::items`] should be processed and their submitters notified.
+    /// See [`Batch::notify_done`], [`Batch::notify_all_done`].
+    Work(Batch<Key, Item, T>),
+    /// The submitted item has been processed in a batch by another submitter
+    /// which has notified the item as done.
+    Done(T),
+    /// The item started batch processing by another submitter but was dropped
+    /// before notifying done.
     Failed,
-    Work(Batch<Key, Item>),
 }
 
-pub struct Batch<Key: Eq + Hash + Clone, Item> {
+/// A batch of items to process.
+pub struct Batch<Key: Eq + Hash + Clone, Item, T> {
+    /// Batch items.
     pub items: Vec<Item>,
     guard: Option<OwnedMutexGuard<()>>,
-    senders: Vec<Sender<Key, Item>>,
-    cleaner: Cleaner<Key, Item>,
+    senders: Vec<Option<Sender<Key, Item, T>>>,
+    cleaner: Cleaner<Key, Item, T>,
+    local_rx: Receiver<Key, Item, T>,
 }
 
-impl<Key: Eq + Hash + Clone, Item> fmt::Debug for Batch<Key, Item>
+impl<Key, Item, T> fmt::Debug for Batch<Key, Item, T>
 where
+    Key: Eq + Hash + Clone,
     Item: fmt::Debug,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -115,21 +237,51 @@ where
     }
 }
 
-impl<Key: Eq + Hash + Clone, Item> Batch<Key, Item> {
-    pub fn notify_all_ok(&mut self) {
-        for tx in self.senders.drain(..) {
-            let _ = tx.send(Ok(self.cleaner.clone()));
+impl<Key: Eq + Hash + Clone, Item, T> Batch<Key, Item, T> {
+    /// Notify an individual item as done.
+    ///
+    /// This means the submitter of that item will receive a [`BatchResult::Done`] with the done value.
+    ///
+    /// If the item is the local item, the one submitted by the same caller handling the batch,
+    /// the result may be received using [`Batch::recv_local_notify_done`].
+    pub fn notify_done(&mut self, item_index: usize, val: T) {
+        if let Some(tx) = self.senders[item_index].take() {
+            let _ = tx.send(Ok((val, self.cleaner.clone())));
         }
+    }
+
+    /// Receive the local item's done notification if available.
+    ///
+    /// The local item is the one submitted by the caller that received [`BatchResult::Work`].
+    ///
+    /// Since the local item is handled along with multiple other non-local items, this method
+    /// can be used to get the local computation result after handling all items and calling
+    /// [`Batch::notify_done`] for each (one of which is the local item).
+    pub fn recv_local_notify_done(&mut self) -> Option<T> {
+        self.local_rx.try_recv().ok().and_then(|v| Some(v.ok()?.0))
     }
 
     fn notify_all_failed(&mut self) {
         for tx in self.senders.drain(..) {
-            let _ = tx.send(Err(self.cleaner.clone()));
+            let _ = tx.map(|tx| tx.send(Err(self.cleaner.clone())));
         }
     }
 }
 
-impl<Key: Eq + Hash + Clone, Item> Drop for Batch<Key, Item> {
+impl<Key: Eq + Hash + Clone, Item> Batch<Key, Item, ()> {
+    /// Notify all items, that have not already been notified, as done.
+    ///
+    /// This means all other submitters will receive [`BatchResult::Done`].
+    ///
+    /// Convenience method when using no/`()` item return value.
+    pub fn notify_all_done(&mut self) {
+        for tx in self.senders.drain(..) {
+            let _ = tx.map(|tx| tx.send(Ok(((), self.cleaner.clone()))));
+        }
+    }
+}
+
+impl<Key: Eq + Hash + Clone, Item, T> Drop for Batch<Key, Item, T> {
     fn drop(&mut self) {
         self.notify_all_failed(); // handle senders before dropping guard
         self.guard.take(); // drop before cleaner
@@ -137,12 +289,12 @@ impl<Key: Eq + Hash + Clone, Item> Drop for Batch<Key, Item> {
 }
 
 /// Handle that will try to clean up uncontended leftover batch state on drop.
-struct Cleaner<Key: Eq + Hash, Item> {
-    queue: Arc<std::sync::Mutex<HashMap<Key, BatchState<Key, Item>>>>,
+struct Cleaner<Key: Eq + Hash, Item, T> {
+    queue: Arc<std::sync::Mutex<BatchMap<Key, Item, T>>>,
     key: Option<Key>,
 }
 
-impl<Key: Eq + Hash + Clone, Item> Clone for Cleaner<Key, Item> {
+impl<Key: Eq + Hash + Clone, Item, T> Clone for Cleaner<Key, Item, T> {
     fn clone(&self) -> Self {
         Self {
             queue: Arc::clone(&self.queue),
@@ -151,7 +303,7 @@ impl<Key: Eq + Hash + Clone, Item> Clone for Cleaner<Key, Item> {
     }
 }
 
-impl<Key: Eq + Hash, Item> Drop for Cleaner<Key, Item> {
+impl<Key: Eq + Hash, Item, T> Drop for Cleaner<Key, Item, T> {
     fn drop(&mut self) {
         // try to cleanup leftover states if possible
         if let Some(key) = self.key.take() {
--- tests/usage.rs
+++ tests/usage.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use benjamin_batchly::{BatchMutex, BatchResult};
 use std::{
     mem,
@@ -10,12 +11,12 @@ async fn batch_all_ok() {
 
     let batch_key = 123;
 
-    let (a, b, c, d, e) = futures::join!(
-        handle_ok(&batcher, batch_key, Item('a')),
-        handle_ok(&batcher, batch_key, Item('b')),
-        handle_ok(&batcher, batch_key, Item('c')),
-        handle_ok(&batcher, batch_key, Item('d')),
-        handle_ok(&batcher, batch_key, Item('e')),
+    let (a, b, c, d, e) = tokio::join!(
+        handle_batch_in_100ms(&batcher, batch_key, Item('a')),
+        handle_batch_in_100ms(&batcher, batch_key, Item('b')),
+        handle_batch_in_100ms(&batcher, batch_key, Item('c')),
+        handle_batch_in_100ms(&batcher, batch_key, Item('d')),
+        handle_batch_in_100ms(&batcher, batch_key, Item('e')),
     );
 
     // First task (a) is unblocked and immediately does it's own work
@@ -30,11 +31,11 @@ async fn batch_all_ok() {
     assert!(b.1 >= a.1);
 
     // since task b handled all remaining tasks they return `Done`
-    assert_eq!(c.0, HandleResult::Done);
+    assert_eq!(c.0, HandleResult::Done(()));
     assert!(c.1 >= b.1);
-    assert_eq!(d.0, HandleResult::Done);
+    assert_eq!(d.0, HandleResult::Done(()));
     assert!(d.1 >= b.1);
-    assert_eq!(e.0, HandleResult::Done);
+    assert_eq!(e.0, HandleResult::Done(()));
     assert!(e.1 >= b.1);
 }
 
@@ -44,16 +45,16 @@ async fn batch_cancelled() {
 
     let batch_key = 123;
 
-    let (a, b, c, d, e) = futures::join!(
-        handle_ok(&batcher, batch_key, Item('a')),
+    let (a, b, c, d, e) = tokio::join!(
+        handle_batch_in_100ms(&batcher, batch_key, Item('a')),
         // cancel b around halfway through
         tokio::time::timeout(
             Duration::from_millis(150),
-            handle_ok(&batcher, batch_key, Item('b'))
+            handle_batch_in_100ms(&batcher, batch_key, Item('b'))
         ),
-        handle_ok(&batcher, batch_key, Item('c')),
-        handle_ok(&batcher, batch_key, Item('d')),
-        handle_ok(&batcher, batch_key, Item('e')),
+        handle_batch_in_100ms(&batcher, batch_key, Item('c')),
+        handle_batch_in_100ms(&batcher, batch_key, Item('d')),
+        handle_batch_in_100ms(&batcher, batch_key, Item('e')),
     );
 
     // First task (a) is unblocked and immediately does it's own work
@@ -68,27 +69,54 @@ async fn batch_cancelled() {
     assert_eq!(e.0, HandleResult::Failed);
 }
 
+#[tokio::test]
+async fn batch_return_value() {
+    let batcher: BatchMutex<_, _, Result<u32, NotEven>> = BatchMutex::default();
+
+    let batch_key = 123;
+
+    let (a, b, c, d, e) = tokio::join!(
+        handle_convert_to_u32_if_even(&batcher, batch_key, Item('a')),
+        handle_convert_to_u32_if_even(&batcher, batch_key, Item('b')),
+        handle_convert_to_u32_if_even(&batcher, batch_key, Item('c')),
+        handle_convert_to_u32_if_even(&batcher, batch_key, Item('d')),
+        handle_convert_to_u32_if_even(&batcher, batch_key, Item('e')),
+    );
+
+    // First task (a) is unblocked and immediately does it's own work
+    assert!(matches!(a, Ok(Err(NotEven(97)))), "{a:?}");
+
+    // task b awaits a then processes the batch
+    assert!(matches!(b, Ok(Ok(98))), "{b:?}");
+
+    // the remaining tasks are all notified by b
+    assert!(matches!(c, Ok(Err(NotEven(99)))), "{c:?}");
+    assert!(matches!(d, Ok(Ok(100))), "{d:?}");
+    assert!(matches!(e, Ok(Err(NotEven(101)))), "{e:?}");
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Item(char);
 
-/// Submit and handle batch in ~100ms
-async fn handle_ok(
+/// Submit and handle batch in ~100ms.
+async fn handle_batch_in_100ms(
     batcher: &BatchMutex<i32, Item>,
     key: i32,
     item: Item,
 ) -> (HandleResult, Instant) {
     match batcher.submit(key, item).await {
-        BatchResult::Done => (HandleResult::Done, Instant::now()),
+        BatchResult::Done(_) => (HandleResult::Done(()), Instant::now()),
         BatchResult::Failed => (HandleResult::Failed, Instant::now()),
         BatchResult::Work(mut batch) => {
             let items = mem::take(&mut batch.items);
+            assert_eq!(items[0], item);
 
             // simuluate some io
             tokio::time::sleep(Duration::from_millis(100)).await;
             let finish = Instant::now();
 
             // notify that each task succeeded
-            batch.notify_all_ok();
+            batch.notify_all_done();
 
             (HandleResult::DidWork(items), finish)
         }
@@ -96,8 +124,42 @@ async fn handle_ok(
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-enum HandleResult {
-    Done,
+enum HandleResult<T = ()> {
+    Done(T),
     Failed,
     DidWork(Vec<Item>),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct NotEven(u32);
+
+/// Submit and handle batch in ~100ms.
+///
+/// Return Ok(u32) for all even converted chars, and an error otherwise.
+async fn handle_convert_to_u32_if_even(
+    batcher: &BatchMutex<i32, Item, Result<u32, NotEven>>,
+    key: i32,
+    item: Item,
+) -> anyhow::Result<Result<u32, NotEven>> {
+    match batcher.submit(key, item).await {
+        BatchResult::Done(v) => Ok(v),
+        BatchResult::Failed => Err(anyhow::anyhow!("BatchResult::Failed")),
+        BatchResult::Work(mut batch) => {
+            let items = mem::take(&mut batch.items);
+
+            // simuluate some io
+            tokio::time::sleep(Duration::from_millis(100)).await;
+
+            for (idx, item) in items.iter().enumerate() {
+                let n = item.0 as u32;
+                if n % 2 == 0 {
+                    batch.notify_done(idx, Ok(n));
+                } else {
+                    batch.notify_done(idx, Err(NotEven(n)));
+                }
+            }
+
+            batch.recv_local_notify_done().context("did not recv local")
+        }
+    }
 }
